0fbdc327beb81b9a209389f52b0e087ef1756954
Tanimou Cisse <tanimou@live.fr>
2022-07-11 18:44:48 +0000

    update 7/11/2022

--- MultipleExcelSheetsToCsv.py
+++ MultipleExcelSheetsToCsv.py
@@ -6,17 +6,19 @@ import openpyxl as xl
 def xls_to_csv(file_path, csv_name):
     wb = xl.load_workbook(file_path)
     for sheet in wb.sheetnames:
-        with open(f'{csv_name}_{sheet.title()}.csv', 'w') as csv_file:
+        with open(f"{csv_name}_{sheet.title()}.csv", "w") as csv_file:
             writer = csv.writer(csv_file)
             xls_sheet = wb[sheet]
-            maxRow = xls_sheet.max_row+1
-            maxCol = xls_sheet.max_column+1
-            headers = (xls_sheet.cell(
-                row=1, column=col).value for col in range(1, maxCol))
+            maxRow = xls_sheet.max_row + 1
+            maxCol = xls_sheet.max_column + 1
+            headers = (
+                xls_sheet.cell(row=1, column=col).value for col in range(1, maxCol)
+            )
             writer.writerow(headers)
             for r in range(2, maxRow):
-                xls_row = (xls_sheet.cell(
-                    row=r, column=col).value for col in range(1, maxCol))
+                xls_row = (
+                    xls_sheet.cell(row=r, column=col).value for col in range(1, maxCol)
+                )
                 writer.writerow(xls_row)
 
 
@@ -41,6 +43,6 @@ with pathlib.Path(file_path) as xls_file:
 
 
 ##Another way to convert only one excel sheet file to csv
-#import pandas as pd
-#read_file=pd.read_excel(r'Path of the file\filename.xlsx')
-#read_file.to_csv(r'path to store the file.csv',index=None,header=True)
+# import pandas as pd
+# read_file=pd.read_excel(r'Path of the file\filename.xlsx')
+# read_file.to_csv(r'path to store the file.csv',index=None,header=True)
--- database_connection with_python.py
+++ database_connection with_python.py
@@ -1,5 +1,5 @@
-#These libraries are pre-installed in SN Labs. If running in another environment please uncomment lines below to install them:
-#pip install --force-reinstall ibm_db==3.1.1 ibm_db_sa==0.3.3
+# These libraries are pre-installed in SN Labs. If running in another environment please uncomment lines below to install them:
+# pip install --force-reinstall ibm_db==3.1.1 ibm_db_sa==0.3.3
 # Ensure we don't load_ext with sqlalchemy>=1.4 (incompadible)
 # pip uninstall sqlalchemy==1.4 -y && pip install sqlalchemy==1.3.24
 # !pip install ipython-sql
@@ -8,20 +8,20 @@ import ibm_db
 import ibm_db_dbi
 import pandas
 
-#Replace the placeholder values with your actual Db2 hostname, username, and password:
+# Replace the placeholder values with your actual Db2 hostname, username, and password:
 # e.g.: "54a2f15b-5c0f-46df-8954-7e38e612c2bd.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud"
 dsn_hostname = "824dfd4d-99de-440d-9991-629c01b3832d.bs2io90l08kqb1od8lcg.databases.appdomain.cloud"
-dsn_uid = "bjy32482"        # e.g. "abc12345"
-dsn_pwd = "Ar4LSFWbBnL8icQ8"      # e.g. "7dBZ3wWt9XN6$o0J"
+dsn_uid = "bjy32482"  # e.g. "abc12345"
+dsn_pwd = "Ar4LSFWbBnL8icQ8"  # e.g. "7dBZ3wWt9XN6$o0J"
 
 dsn_driver = "{IBM DB2 ODBC DRIVER}"
-dsn_database = "BLUDB"            # e.g. "BLUDB"
-dsn_port = "30119"                # e.g. "32733"
-dsn_protocol = "TCPIP"            # i.e. "TCPIP"
+dsn_database = "BLUDB"  # e.g. "BLUDB"
+dsn_port = "30119"  # e.g. "32733"
+dsn_protocol = "TCPIP"  # i.e. "TCPIP"
 dsn_security = "SSL"  # i.e. "SSL"
 
-#DO NOT MODIFY THIS CELL. Just RUN it with Shift + Enter
-#Create the dsn connection string
+# DO NOT MODIFY THIS CELL. Just RUN it with Shift + Enter
+# Create the dsn connection string
 dsn = (
     "DRIVER={0};"
     "DATABASE={1};"
@@ -30,66 +30,86 @@ dsn = (
     "PROTOCOL={4};"
     "UID={5};"
     "PWD={6};"
-    "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd, dsn_security)
+    "SECURITY={7};"
+).format(
+    dsn_driver,
+    dsn_database,
+    dsn_hostname,
+    dsn_port,
+    dsn_protocol,
+    dsn_uid,
+    dsn_pwd,
+    dsn_security,
+)
 
-#print the connection string to check correct values are specified
-#print(dsn)
+# print the connection string to check correct values are specified
+# print(dsn)
 
-#DO NOT MODIFY THIS CELL. Just RUN it with Shift + Enter
-#Create database connection
+# DO NOT MODIFY THIS CELL. Just RUN it with Shift + Enter
+# Create database connection
 
 try:
     conn = ibm_db.connect(dsn, "", "")
-    print("Connected to database: ", dsn_database,
-          "as user: ", dsn_uid, "on host: ", dsn_hostname)
+    print(
+        "Connected to database: ",
+        dsn_database,
+        "as user: ",
+        dsn_uid,
+        "on host: ",
+        dsn_hostname,
+    )
 
 except Exception:
     print("Unable to connect: ", ibm_db.conn_errormsg())
 
 dropQuery = "drop  table INSTRUCTOR"
 
-#Now execute the drop statment
-#dropStmt = ibm_db.exec_immediate(conn, dropQuery)
+# Now execute the drop statment
+# dropStmt = ibm_db.exec_immediate(conn, dropQuery)
 
 
-#Construct the Create Table DDL statement - replace the ... with rest of the statement
+# Construct the Create Table DDL statement - replace the ... with rest of the statement
 createQuery = "create table INSTRUCTOR(id INTEGER PRIMARY KEY NOT NULL, fname VARCHAR(20) NULL,lname VARCHAR(20) NULL,city VARCHAR(20) NULL,ccode CHARACTER(2) NULL)"
 
-#Now fill in the name of the method and execute the statement
-#createStmt = ibm_db.exec_immediate(conn, createQuery)
+# Now fill in the name of the method and execute the statement
+# createStmt = ibm_db.exec_immediate(conn, createQuery)
 
-#Construct the query - replace ... with the insert statement
+# Construct the query - replace ... with the insert statement
 insertQuery = "insert into INSTRUCTOR(id,fname,lname,city,ccode) values(1,'Rav','Ahuja','TORONTO','CA')"
 insertQuery2 = "insert into INSTRUCTOR(id,fname,lname,city,ccode) values(2,'Raul','Chong','Markham','CA'),(3,'ima','Vasudevan','Chicago','US')"
 
 
-#execute the insert statement
-#insertStmt = ibm_db.exec_immediate(conn, insertQuery2)
+# execute the insert statement
+# insertStmt = ibm_db.exec_immediate(conn, insertQuery2)
 
-#Construct the query that retrieves all rows from the INSTRUCTOR table
+# Construct the query that retrieves all rows from the INSTRUCTOR table
 selectQuery = "select * from INSTRUCTOR"
 
-#Execute the statement
+# Execute the statement
 selectStmt = ibm_db.exec_immediate(conn, selectQuery)
 
-#Fetch the Dictionary (for the first row only)
+# Fetch the Dictionary (for the first row only)
 ibm_db.fetch_both(selectStmt)
 
-#Fetch the rest of the rows and print the ID and FNAME for those rows
+# Fetch the rest of the rows and print the ID and FNAME for those rows
 while ibm_db.fetch_row(selectStmt) != False:
-   print(" ID:",  ibm_db.result(selectStmt, 0),
-         " FNAME:",  ibm_db.result(selectStmt, "FNAME"))
+    print(
+        " ID:",
+        ibm_db.result(selectStmt, 0),
+        " FNAME:",
+        ibm_db.result(selectStmt, "FNAME"),
+    )
 
-#connection for pandas
+# connection for pandas
 pconn = ibm_db_dbi.Connection(conn)
 
-#retrieve the query results into a pandas dataframe
+# retrieve the query results into a pandas dataframe
 pdf = pandas.read_sql(selectQuery, pconn)
 
-#print just the LNAME for first row in the pandas data frame
+# print just the LNAME for first row in the pandas data frame
 print(pdf.LNAME[0])
 
-#print the entire data frame
+# print the entire data frame
 print(pdf)
 
 ibm_db.close(conn)
